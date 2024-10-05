25b5ba7a5eabe34ef209cdcfc2366bd82494d07c
Lunar <jhonathaornellas1227@gmail.com>
2024-10-04 23:23:02 -0300

    first commit

---
+++ src/app/sobre/sobre.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre',
+  templateUrl: './sobre.component.html',
+  styleUrls: ['./sobre.component.scss'],
+})
+export class SobreComponent {
+  img = 'assets/img.pdf Jhon.jpg';
+  turma = 'ADS';
+  turno = 'Manhã';
+  unidade = 'Unidade Bosucesso';
+  alunoNome = 'Jhonatha de Ornellas lins';
+}
+
+
+
